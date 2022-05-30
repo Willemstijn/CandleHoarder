37474c13bb28cd5c8b50327eddf41c396cf402c5
bas <willemstijn@gmail.com>
2022-05-30 06:51:52 -0400

    Added original engine code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# CandleHoarder
-Engine to grab cryptocurrency candle data
+Read: 
+
+* https://python-binance.readthedocs.io/en/latest/overview.html
+* https://python-binance.readthedocs.io/en/latest/constants.html
+
+For information about the python-binance library.
---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# This is the config file for the Opportunity knocks warning bot.
+
+# Enter all the crypto pairs you want to watch in this array.
+symbols = ["BTCUSDT", "ETHUSDT", "ADAUSDT", "XRPUSDT"]
+
+# Select a timeframe to watch on.
+# Available timeframes are: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M.
+time_frames = [
+    "1h",
+    "4h",
+    "1d",
+    "3d",
+    "1w",
+]
+
+# Binance keys - KEEP THIS SECRET!! -
+API_KEY = "7x45RaKCuKjwHUaWX1FqljGhAYvSYiE9QRJUD4DLMCqoJULYtPYXNFDKdSadUkjm"
+API_SECRET = "xrb9aNTzUDQ34Oy4bYX1eMQZsDpbS0bJT4uSMTPAb3O17vvyaDtX2kur9PcS6S0M"
