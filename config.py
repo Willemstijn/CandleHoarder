0f34ce6e44161191354ee718bc79402ec232be72
Bas Willemstijn <bas.willemstijn@mn.nl>
2022-06-02 14:59:23 +0200

    Added several notebooks that use SQL as database source

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ data_location = "./data/"
 
 # Enter all the crypto pairs you want to watch in this array.
 # symbols = ["BTCUSDT", "ETHUSDT", "ADAUSDT", "XRPUSDT"]
-symbols = ["BTCUSDT","ETHUSDT","ADAUSDT","XRPUSDT"]
+symbols = ["BTCUSDT"]
 
 # Select a timeframe to watch on.
 # Available timeframes are: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M.
-time_frames = ["1d","4h"]
+time_frames = ["1d"]
 
 # Enter amount of historical data to fetch for initial candle data download
-candle_history = 10
+candle_history = 1000
 
 # Client key & secret for downloading data through the API exchange
 client = Client(secrets.BINANCE_API_KEY, secrets.BINANCE_API_SECRET)
