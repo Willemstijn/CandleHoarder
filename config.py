ca59755f78034084606f241d4f82edbb2b7998d3
bas@tatooine <bas@willemstijn.net>
2022-06-18 09:57:07 +0200

    Final commit for this stable branch. The bot works good!

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ import secrets
 dir = "/opt/CandleHoarder/"
 
 # Enter the location where the databases should exist be created.
-# data_location = dir+"data/"
-data_location = "./data/"
+data_location = dir+"data/"
+# data_location = "./data/"
 
 # Select a timeframe to watch on.
 # Available timeframes are: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M.
